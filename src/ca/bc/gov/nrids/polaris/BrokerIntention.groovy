c62d9806f2d933af3f1de6925461c1f4d315457f
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2023-09-28 09:20:26 -0700

    Add libraries

---
+++ src/ca/bc/gov/nrids/polaris/BrokerIntention.groovy
@@ -0,0 +1,232 @@
+package ca.bc.gov.nrids.polaris
+
+class BrokerIntention implements Serializable {
+  Object intention
+  Object openResponse
+  static final String BROKER_BASE_URL = "https://nr-broker.apps.silver.devops.gov.bc.ca/v1/"
+  static final String HEADER_BROKER_TOKEN = "X-Broker-Token"
+
+  BrokerIntention(Object intention) {
+    this.intention = intention
+  }
+
+  /**
+   * Reads the intention json from the path and returns a new BrokerIntention.
+   * - Positional parameters
+   * path   String  The path to the file to read
+   */
+  static BrokerIntention fromFile(String path) {
+    def jsonSlurper = new groovy.json.JsonSlurperClassic()
+    return new BrokerIntention(jsonSlurper.parseText(new File(path).text))
+  }
+
+  // Convenience getter/setter for user id & name, event url and event provider
+  def getUserId() {
+    return intention.user.id
+  }
+  def setUserId(user) {
+    intention.user.id = (String) user
+  }
+
+  def getUserName() {
+    return intention.user.name
+  }
+  def setUserName(user) {
+    intention.user.name = (String) user
+  }
+
+  def getEventUrl() {
+    return intention.event.url
+  }
+  def setEventUrl(url) {
+    intention.event.url = (String) url
+  }
+
+  def getEventProvider() {
+    return intention.event.provider
+  }
+  def setEventProvider(provider) {
+    intention.event.provider = (String) provider
+  }
+
+  /**
+   * Open the intention
+   * - Positional parameters
+   * authToken   String   The JWT to send to authenticate this request
+   * - Named parameters
+   * ttl:        Number   The time to live (ttl) for the intention.
+   *                      Long running processes may need to set this higher than the default.
+   */
+  public boolean open(Map args, String authToken) {
+    def jsonSlurper = new groovy.json.JsonSlurperClassic()
+
+    if (!authToken) {
+      throw new IllegalArgumentException()
+    }
+
+    // POST open
+    def params = args.ttl ? "?ttl=${args.ttl}" : ""
+    def post = new URL(this.BROKER_BASE_URL + "intention/open" + params).openConnection()
+    def message = groovy.json.JsonOutput.toJson(intention)
+    post.setRequestMethod("POST")
+    post.setDoOutput(true)
+    post.setRequestProperty("Content-Type", "application/json")
+    post.setRequestProperty("Authorization", "Bearer " + authToken)
+    post.getOutputStream().write(message.getBytes("UTF-8"))
+    def postRC = post.getResponseCode()
+    if (this.isResponseSuccess(postRC)) {
+      this.openResponse = jsonSlurper.parseText(post.getInputStream().getText())
+      return true
+    }
+    throw new IllegalStateException(post.getInputStream().getText())
+  }
+  def open(String authToken) {
+    this.open([:], authToken)
+  }
+
+  /**
+   * Start the action
+   * - Positional parameters
+   * action   String  The action id from the intention
+   */
+  public void startAction(String action) {
+    if (!action) {
+      throw new IllegalArgumentException()
+    }
+    if (!this.openResponse) {
+      throw new IllegalStateException()
+    }
+    this.actionLifecycleLog(this.openResponse.actions[action].token, "start")
+  }
+
+  /**
+   * End the action
+   * - Positional parameters
+   * action   String  The action id from the intention
+   */
+  public void endAction(String action) {
+    if (!action) {
+      throw new IllegalArgumentException()
+    }
+    if (!this.openResponse) {
+      throw new IllegalStateException()
+    }
+    this.actionLifecycleLog(this.openResponse.actions[action].token, "end")
+  }
+
+  public boolean actionLifecycleLog(String token, String type) {
+    def post = new URL(this.BROKER_BASE_URL + "intention/action/" + type).openConnection()
+    post.setRequestMethod("POST")
+    post.setRequestProperty("Content-Type", "application/json")
+    post.setRequestProperty(HEADER_BROKER_TOKEN, token)
+    return this.isResponseSuccess(post.getResponseCode())
+  }
+
+  /**
+   * Close the intention
+   * - Positional parameters
+   * successArg   boolean  Set true if all actions were a success
+   */
+  public String close(boolean successArg) {
+    // Ignore close if there is no open response
+    if (!this.openResponse) {
+      throw new IllegalStateException("Intention was never opened")
+    }
+    // POST close
+    def jsonSlurper = new groovy.json.JsonSlurperClassic()
+    def post = new URL(
+      this.BROKER_BASE_URL + "intention/close?outcome=" +
+      (successArg ? "success": "failure")).openConnection()
+    post.setRequestMethod("POST")
+    post.setRequestProperty("Content-Type", "application/json")
+    post.setRequestProperty(HEADER_BROKER_TOKEN, this.openResponse.token)
+    def resp = jsonSlurper.parseText(post.getInputStream().getText())
+    return resp.audit
+  }
+
+  /**
+   * Provision a secret id that can be used by the service
+   * to login as the service to vault.
+   * - Positional parameters
+   * action   String  The action id from the intention
+   * roleId   String  The service role id
+   *
+   * Note: The return value is wrapper token that should be
+   * unwrapped only on the server using it.
+   *
+   * Unwrap example: vault unwrap -field=secret_id <token>
+   */
+  public String provisionSecretId(action, roleId = null) {
+    if (!action) {
+      throw new IllegalArgumentException()
+    }
+    if (!this.openResponse) {
+      throw new IllegalStateException()
+    }
+    def actionToken = this.openResponse?.actions[action].token
+    if (!actionToken) {
+      throw new IllegalArgumentException()
+    }
+    def jsonSlurper = new groovy.json.JsonSlurperClassic()
+    def post = new URL(this.BROKER_BASE_URL + "provision/approle/secret-id").openConnection()
+    post.setRequestMethod("POST")
+    post.setRequestProperty("Content-Type", "application/json")
+    if (roleId) {
+      post.setRequestProperty("X-Vault-Role-Id", (String) roleId)
+    }
+    post.setRequestProperty(HEADER_BROKER_TOKEN, (String) actionToken)
+
+    if (!this.isResponseSuccess(post.getResponseCode())) {
+      throw new IllegalStateException(post.getInputStream().getText())
+    }
+    def wrappedTokenResponse = jsonSlurper.parseText(post.getInputStream().getText())
+    return wrappedTokenResponse.wrap_info.token
+  }
+
+  /**
+   * Provision a (temporary) token to use with vault.
+   * - Positional parameters
+   * action       String  The action id from the intention
+   * roleId       String  The service role id
+   * unwrapToken  Boolean True if the token should be unwrapped for you.
+   *
+   * Note: The optional unwrapToken must be set false if the caller does
+   * use the token directly. Unwrapped token must never be saved to disk or
+   * transmitted.
+   */
+  public String provisionToken(action, roleId = null, unwrapToken = true) {
+    if (!action) {
+      throw new IllegalArgumentException()
+    }
+    if (!this.openResponse) {
+      throw new IllegalStateException()
+    }
+    def actionToken = this.openResponse?.actions[action].token
+    if (!actionToken) {
+      throw new IllegalArgumentException()
+    }
+    def jsonSlurper = new groovy.json.JsonSlurperClassic()
+    def post = new URL(this.BROKER_BASE_URL + "provision/token/self").openConnection()
+    post.setRequestMethod("POST")
+    post.setRequestProperty("Content-Type", "application/json")
+    if (roleId) {
+      post.setRequestProperty("X-Vault-Role-Id", (String) roleId)
+    }
+    post.setRequestProperty(HEADER_BROKER_TOKEN, (String) actionToken)
+
+    if (!this.isResponseSuccess(post.getResponseCode())) {
+      throw new IllegalStateException(post.getInputStream().getText())
+    }
+    def wrappedTokenResponse = jsonSlurper.parseText(post.getInputStream().getText())
+
+    if (!unwrapToken) {
+      return wrappedTokenResponse.wrap_info.token
+    }
+
+    return Vault.unwrapToken(wrappedTokenResponse.wrap_info.token)
+  }
+
+  private boolean isResponseSuccess(code) {
+    return code >= 200 && code <= 299
+  }
+}
---
+++ src/ca/bc/gov/nrids/polaris/Podman.groovy
@@ -0,0 +1,160 @@
+package ca.bc.gov.nrids.polaris
+
+class Podman implements Serializable {
+  def steps
+  def env
+  String imagePrefix
+  String authfile = ".docker.config.json"
+
+  Podman(steps, env = null, String imagePrefix = "artifacts.developer.gov.bc.ca/docker-remote") {
+    this.steps = steps
+    this.imagePrefix = imagePrefix
+    this.env = env ? env.getEnvironment() : null
+  }
+
+  /**
+   * Podman build command
+   * - Positional parameters
+   * context          string  The specified build context directory
+   * - Named parameters
+   * tag:             string  Tag name to apply to the build image
+   * options:         string  Additional options
+   * authfile:        string  If set, override the default authfile
+   */
+  def build(Map args, String context) {
+    def shellCmd = (args.httpProxy ? "HTTP_PROXY=${args.httpProxy} " : "") +
+      "podman build --authfile=${args.authfile ?: authfile} ${args.options ?: ''} ${args.tag ? '--tag ' + args.tag : ''} ${context}";
+    steps.sh shellCmd
+  }
+  def build(Map args) {
+    this.build(args, '')
+  }
+  def build(String context) {
+    this.build([:], context)
+  }
+
+  /**
+   * Podman login command
+   * - Named parameters
+   * httpProxy:       string  If set, sets HTTP_PROXY for podman command
+   * options:         string  Run arguments like '--authfile=/path/to/auth.json'
+   * authfile:        string  If set, override the default authfile
+   *
+   * - Registry credentials
+   * For login purposes, store registry credentials in Vault and use the Vault class to
+   * retrieve them. For example:
+   *
+   * def vault = new Vault(vaultToken)
+   * def registryCreds = vault.read('apps/data/prod/path/to/registry/credentials')
+   * env.REGISTRY_USERNAME = registryCreds['username']
+   * env.REGISTRY_PASSWORD = registryCreds['password']
+   *
+   * Note: The Vault path must include "data" after the secrets engine mount point.
+   *
+   */
+  def login(Map args) {
+    if (args.authfile) {
+      authfile = args.authfile
+    }
+    def shellCmd = (args.httpProxy ? "HTTP_PROXY=${args.httpProxy} " : "") +
+      "set +x ; podman login --authfile=${authfile} ${args.options ?: ''} ${args.registry ?: imagePrefix}";
+    steps.sh shellCmd
+  }
+
+  /**
+   * Podman logout command
+   * - Named parameters
+   * httpProxy:       string  If set, sets HTTP_PROXY for podman command
+   * options:         string  Run arguments like '--authfile=/path/to/auth.json'
+   * authfile:        string  If set, override the default authfile
+   */
+  def logout(Map args) {
+    def shellCmd = (args.httpProxy ? "HTTP_PROXY=${args.httpProxy} " : "") +
+      "podman logout --authfile=${args.authfile ?: authfile} ${args.options ?: ''} --all";
+    steps.sh shellCmd
+  }
+
+  /**
+   * Podman pull command
+   * - Positional parameters
+   * imageId          string  The name of the container image to run. Combined with image prefix if set in constructor
+   * - Named parameters
+   * options:         string  Run arguments like '-v \$(pwd)/fb/config:/app/config'
+   * httpProxy:       string  If set, sets HTTP_PROXY for podman command
+   * imagePrefix:     string  If set, override image prefix set in constructor
+   * authfile:        string  If set, override the default authfile
+   */
+  def pull(Map args, String imageId) {
+    def shellCmd = (args.httpProxy ? "HTTP_PROXY=${args.httpProxy} " : "") +
+      "podman pull --authfile=${args.authfile ?: authfile} ${args.options ?: ''} ${renderImageId(args, imageId)}";
+    steps.sh shellCmd
+  }
+  def pull(String imageId) {
+    this.pull([:], imageId)
+  }
+
+  /**
+   * Podman push command
+   * - Positional parameters
+   * imageId          string  The name of the container image to run. Combined with image prefix if set in constructor
+   * - Named parameters
+   * httpProxy:       string  If set, sets HTTP_PROXY for podman command
+   * imagePrefix:     string  If set, override image prefix set in constructor
+   * username         string  The username to use to authenticate with the registry
+   * password         string  The password to use to authenticate with the registry
+   * authfile:        string  If set, override the default authfile
+   */
+  def push(Map args, String imageId) {
+    def shellCmd = (args.httpProxy ? "HTTP_PROXY=${args.httpProxy} " : "") +
+      "podman push --authfile=${args.authfile ?: authfile} ${imageId} ${renderImageId(args, imageId)}";
+    steps.sh shellCmd
+  }
+
+  /**
+   * Podman run command
+   * - Positional parameters
+   * imageId          string  The name of the container image to run. Combined with image prefix if set in constructor
+   * - Named parameters
+   * command:         string  Overrive contianer command (and any options).
+   * options:         string  Run arguments like '-v \$(pwd)/fb/config:/app/config'
+   * httpProxy:       string  If set, sets HTTP_PROXY for podman command
+   * imagePrefix:     string  If set, override image prefix set in constructor
+   * returnStdout:    bool    If true, return standard out
+   * skipPipelineEnv: bool    If true (or if env in constructor was null), do not automatically add all environment variables
+   * authfile:        string  If set, override the default authfile
+   */
+  def run(Map args, String imageId) {
+    def toEnvOptions = {
+      it.collect { "-e ${it.key}=${it.value.replaceAll(' ', '\\\\ ')}" } join " "
+    }
+    def envOpts = this.env && !args.skipPipelineEnv ? toEnvOptions(this.env) : ''
+    def shellCmd = (args.httpProxy ? "HTTP_PROXY=${args.httpProxy} " : "") +
+      "podman run --rm --authfile=${args.authfile ?: authfile} ${args.options ?: ''} ${envOpts} ${renderImageId(args, imageId)} ${args.command ?: ''}"
+    if (args.returnStdout)
+      return steps.sh(script: shellCmd, returnStdout: true)
+    else {
+      steps.sh shellCmd
+    }
+  }
+  def run(String imageId) {
+    this.run([:], imageId)
+  }
+
+  /**
+   * Podman version command
+   */
+  def version(Map args) {
+    steps.sh "podman version"
+  }
+  def version() {
+    this.version([:])
+  }
+
+  /**
+   * Private: Renders image prefix
+   */
+  def renderImageId(Map args, String imageId) {
+    def prefix = args.imagePrefix ?: imagePrefix
+    return (prefix ? prefix + '/' : '') + imageId
+  }
+}
