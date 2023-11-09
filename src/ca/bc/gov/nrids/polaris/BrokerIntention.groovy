56160b1575065b5d021bb0131d52330640b2deda
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2023-11-09 09:45:23 -0800

    Remove server access test

--- src/ca/bc/gov/nrids/polaris/BrokerIntention.groovy
+++ src/ca/bc/gov/nrids/polaris/BrokerIntention.groovy
@@ -70,9 +70,7 @@ class BrokerIntention implements Serializable {
     }
     if (args.environment) {
       for (action in this.intention.actions) {
-        if (action.action != "server-access") {
-          action.service.environment = args.environment
-        }        
+        action.service.environment = args.environment
       }
     }
     if (args.packageInstallationVersion) {
