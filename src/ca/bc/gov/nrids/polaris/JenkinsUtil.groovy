5cb7050777e70502f529d6c2fef12f5a34627853
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2024-05-17 16:07:29 -0700

    feat: get upstream cause user id

--- src/ca/bc/gov/nrids/polaris/JenkinsUtil.groovy
+++ src/ca/bc/gov/nrids/polaris/JenkinsUtil.groovy
@@ -25,6 +25,25 @@ class JenkinsUtil implements Serializable {
   }
 
   /**
+   * Get NR Broker compatible cause user id from upstream build
+   * - Positional parameters
+   * build          object  The global currentBuild object is expected
+   * defaultUser    string  The default user id
+   */
+  static String getUpstreamCauseUserId(build, defaultUser = "unknown") {
+    final hudson.model.Cause$UpstreamCause upstreamCause = build.rawBuild.getCause(hudson.model.Cause$UpstreamCause)
+    final hudson.model.Cause$UserIdCause userIdCause = upstreamCause == null ?
+        build.rawBuild.getCause(hudson.model.Cause$UserIdCause) :
+        upstreamCause.getUpstreamRun().getCause(hudson.model.Cause$UserIdCause)
+    final String nameFromUserIdCause = userIdCause != null ? userIdCause.userId : null
+    if (nameFromUserIdCause != null) {
+        return nameFromUserIdCause + "@azureidir"
+    } else {
+        return defaultUser ? defaultUser : 'unknown'
+    }
+  }
+
+  /**
    * Converts a standard long environment name into its short version
    * - Positional parameters
    * env          string  The environment name to convert
@@ -45,14 +64,14 @@ class JenkinsUtil implements Serializable {
       String encodedAuth = Base64.getEncoder().encodeToString(auth.bytes)
       String authHeader = "Basic ${encodedAuth}"
       connection.setRequestProperty("Authorization", authHeader)
-      
+
       connection.setRequestMethod("POST")
       connection.setRequestProperty("Content-Type", "application/octet-stream")
       connection.setDoOutput(true)
-    
+
       OutputStream os = connection.getOutputStream()
       byte[] input = encodeBody.getBytes("utf-8")
-      os.write(input, 0, input.length)    
+      os.write(input, 0, input.length)
 
       def responseCode = connection.getResponseCode()
 
