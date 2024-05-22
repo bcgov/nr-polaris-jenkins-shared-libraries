a7b5337bb63722a8dabf170f639967988444b069
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2024-05-22 11:13:50 -0700

    feat: automatedb migration (#20)
    * feat: get upstream cause user id
    
    * feat: add service name and project
    
    * feat: add short to long

--- src/ca/bc/gov/nrids/polaris/JenkinsUtil.groovy
+++ src/ca/bc/gov/nrids/polaris/JenkinsUtil.groovy
@@ -8,6 +8,12 @@ class JenkinsUtil implements Serializable {
     development: "dev"
   ]
 
+  private final static def envShortToLong = [
+    prod: "production",
+    test: "test",
+    dev: "development"
+  ]
+
   /**
    * Get NR Broker compatible cause user id from build (currentBuild)
    * - Positional parameters
@@ -25,12 +31,40 @@ class JenkinsUtil implements Serializable {
   }
 
   /**
+   * Get NR Broker compatible cause user id from upstream build
+   * - Positional parameters
+   * build          object  The global currentBuild object is expected
+   * defaultUser    string  The default user id
+   */
+  static String getUpstreamCauseUserId(build, defaultUser = "unknown") {
+    final hudson.model.Cause$UpstreamCause upstreamCause = build.rawBuild.getCause(hudson.model.Cause$UpstreamCause)
+    final hudson.model.Cause$UserIdCause userIdCause = upstreamCause == null ?
+        build.rawBuild.getCause(hudson.model.Cause$UserIdCause) :
+        upstreamCause.getUpstreamRun().getCause(hudson.model.Cause$UserIdCause)
+    final String nameFromUserIdCause = userIdCause != null ? userIdCause.userId : null
+    if (nameFromUserIdCause != null) {
+        return nameFromUserIdCause + "@azureidir"
+    } else {
+        return defaultUser ? defaultUser : 'unknown'
+    }
+  }
+
+  /**
    * Converts a standard long environment name into its short version
    * - Positional parameters
    * env          string  The environment name to convert
    */
   static String convertLongEnvToShort(env) {
     return JenkinsUtil.envLongToShort[env]
+  }
+
+  /**
+   * Converts a standard short environment name into its long version
+   * - Positional parameters
+   * env          string  The environment name to convert
+   */
+  static String convertShortEnvToLong(env) {
+    return JenkinsUtil.envShortToLong[env]
   }
 
   static void putFile(username, password, apiURL, filePath) {
@@ -45,14 +79,14 @@ class JenkinsUtil implements Serializable {
       String encodedAuth = Base64.getEncoder().encodeToString(auth.bytes)
       String authHeader = "Basic ${encodedAuth}"
       connection.setRequestProperty("Authorization", authHeader)
-      
+
       connection.setRequestMethod("POST")
       connection.setRequestProperty("Content-Type", "application/octet-stream")
       connection.setDoOutput(true)
-    
+
       OutputStream os = connection.getOutputStream()
       byte[] input = encodeBody.getBytes("utf-8")
-      os.write(input, 0, input.length)    
+      os.write(input, 0, input.length)
 
       def responseCode = connection.getResponseCode()
 
