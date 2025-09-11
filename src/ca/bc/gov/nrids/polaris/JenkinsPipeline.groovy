58b3a958a6787520cdfdc6a97d9285d0fa96eaed
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2025-09-11 16:47:19 -0700

    fix script

--- src/ca/bc/gov/nrids/polaris/JenkinsPipeline.groovy
+++ src/ca/bc/gov/nrids/polaris/JenkinsPipeline.groovy
@@ -2,9 +2,15 @@ package ca.bc.gov.nrids.polaris
 
 class JenkinsPipeline implements Serializable {
 
-  static String retrieveInventory(Map config = [:]) {
-      stage('Checkout INFRA dev-all-in-one') {
-        steps {
+  def script
+
+  JenkinsPipeline(def script) {
+    this.script = script
+  }
+
+  def retrieveInventory(Map config = [:]) {
+      script.stage('Checkout INFRA dev-all-in-one') {
+        script.steps {
           checkout([
             $class: 'GitSCM',
             branches: [[name: config.branch ?: 'release/1.0.0']],
