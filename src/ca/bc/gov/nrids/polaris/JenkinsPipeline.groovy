8acee01e73b7eed545a121a2ed950f4d88e102b7
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2025-09-11 16:53:58 -0700

    fix script

--- src/ca/bc/gov/nrids/polaris/JenkinsPipeline.groovy
+++ src/ca/bc/gov/nrids/polaris/JenkinsPipeline.groovy
@@ -9,24 +9,22 @@ class JenkinsPipeline implements Serializable {
   }
 
   def retrieveInventory(Map config = [:]) {
-      script.stage('Checkout INFRA dev-all-in-one') {
-        script.steps {
-          checkout([
-            $class: 'GitSCM',
-            branches: [[name: config.branch ?: 'release/1.0.0']],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [
-              [$class: 'RelativeTargetDirectory', relativeTargetDir: "infra"]
-            ],
-            submoduleCfg: [],
-            userRemoteConfigs: [
-              [
-                credentialsId: 'ci-user',
-                url: "https://bwa.nrs.gov.bc.ca/int/stash/scm/infra/dev-all-in-one.git"
-              ]
-            ]
-        ])
-      }
+    script.stage('Checkout INFRA dev-all-in-one') {
+      script.checkout([
+        $class: 'GitSCM',
+        branches: [[name: config.branch ?: 'release/1.0.0']],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [
+          [$class: 'RelativeTargetDirectory', relativeTargetDir: "infra"]
+        ],
+        submoduleCfg: [],
+        userRemoteConfigs: [
+          [
+            credentialsId: 'ci-user',
+            url: "https://bwa.nrs.gov.bc.ca/int/stash/scm/infra/dev-all-in-one.git"
+          ]
+        ]
+      ])
     }
   }
 }
